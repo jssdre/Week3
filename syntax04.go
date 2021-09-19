c28ed8281432b3cdb310c288a74fdda5b73fd213
jssdre <jsshin1607@gmail.com>
2021-09-19 23:27:38 +0900

    v0.3

--- syntax04.go
+++ syntax04.go
@@ -6,25 +6,25 @@ import (
 	"time"
 )
 
-//난수 추출된 수의 소수 판정 프로그램 v0.2
+//난수 추출된 수의 소수 판정 프로그램 v0.3
 //소수는 1과 자기 자신 외에 나누어 떨어지지 않음
 func main() {
-	// seed 설정
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 
-	count := 0
-	number := rand.Intn(150) + 2 // 0, 1 제외 . 2~152
+	isPrime := true
+	number := rand.Intn(150) + 2
 	fmt.Println("임의로 추출된 수 : ", number)
 
-	for i := 2; i < number; i++ { //1과 number일 때 loop 돌지 않음
+	for i := 2; i < number; i++ {
 		if number%i == 0 {
+			isPrime = false
 			//count++
-			count = count + 1
+			//count = count+1
 		}
 	}
 
-	if count == 0 {
+	if isPrime == true {
 		fmt.Println(number, "는(은) 소수입니다")
 	} else {
 		fmt.Println(number, "는(은) 소수가 아닙니다")
